77534af3bc3c8b5efb665cfb4a8fe1899c331eb5
Serg9t <ssokurr@mail.ru>
2023-01-04 16:01:41 +0300

    added new file

---
+++ The game X-0/the game X-0.py
@@ -0,0 +1,96 @@
+# Приветствие и вводная инструкция
+def greet():
+    print('''
+-------------------
+  Приветсвуем вас  
+******в игре*******
+*крестики - нолики*
+-------------------
+ формат ввода: x y 
+ x - номер строки  
+ y - номер столбца ''')
+
+
+# Создаем игровое поле
+def playing_field():
+    print()
+    print("    | 0 | 1 | 2 | ")
+    print("  --------------- ")
+    for i, row in enumerate(field):
+        row_str = f"  {i} | {' | '.join(row)} | "
+        print(row_str)
+        print("  --------------- ")
+    print()
+
+
+# Запрашиваем у игрока ввод координат и проверяем корректность ввода
+def player_input():
+    while True:
+        cords = input("         Ваш ход: ").split()
+
+        if len(cords) != 2:
+            print(" Введите 2 координаты! ")
+            continue
+
+        x, y = cords
+
+        if not (x.isdigit()) or not (y.isdigit()):
+            print(" Введите числа! ")
+            continue
+
+        x, y = int(x), int(y)
+
+        if 0 > x or x > 2 or 0 > y or y > 2:
+            print(" Координаты вне диапазона! ")
+            continue
+
+        if field[x][y] != " ":
+            print(" Клетка занята! ")
+            continue
+
+        return x, y
+
+
+# Проверяем все возможные комбинации выигрышных позиций
+def check_win():
+    win_cord = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
+                ((0, 2), (1, 1), (2, 0)), ((0, 0), (1, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
+                ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)))
+    for cord in win_cord:
+        symbols = []
+        for c in cord:
+            symbols.append(field[c[0]][c[1]])
+            if symbols == ["X", "X", "X"]:
+                print("Выиграл X!!!")
+                return True
+            if symbols == ["0", "0", "0"]:
+                print("Выиграл 0!!!")
+                return True
+    return False
+
+
+# Итоговое исполнение функций в цикле для игрового процесса
+greet()
+field = [[" "] * 3 for i in range(3)]
+count = 0
+while True:
+    count += 1
+    playing_field()
+    if count % 2 == 1:
+        print(" Ходит крестик!")
+    else:
+        print(" Ходит нолик!")
+
+    x, y = player_input()
+
+    if count % 2 == 1:
+        field[x][y] = "X"
+    else:
+        field[x][y] = "0"
+
+    if check_win():
+        break
+
+    if count == 9:
+        print(" Ничья!")
+        break
